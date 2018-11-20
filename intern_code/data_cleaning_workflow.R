5c5115b6dd8b5c6000a992da0462b3b9e34282be
babichmorrowc <babichmorrowc@kenyon.edu>
2018-11-20 14:56:39 -0500

    adding data cleaning workflow script

---
+++ intern_code/data_cleaning_workflow.R
@@ -0,0 +1,13 @@
+# Your name + your partner's name
+# 11/20/18
+# Spatial cleaning workflow code
+
+
+# Required packages -------------------------------------------------------
+
+# Load the packages you need for your analyses
+# You can copy and paste the packages loaded from 
+
+# Import data -------------------------------------------------------------
+
+
