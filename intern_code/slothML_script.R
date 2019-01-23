0f19002a7d36fad7ae59553c4645c6f30f6513f8
babichmorrowc <babichmorrowc@kenyon.edu>
2019-01-23 16:28:08 -0500

    add slothML_script.R

---
+++ intern_code/slothML_script.R
@@ -0,0 +1,30 @@
+#Script for machine learning with sloth skull data
+
+
+# SUPERVISED LEARNING -----------------------------------------------------
+
+# Load the caret package
+
+# Import the dataset sloth_landmarks.csv (in the lesson_plans/machine_learning folder)
+# Make sure to import "From Text (base)"
+
+
+# View the data
+
+
+# Exploratory data analysis -----------------------------------------------
+
+# Refer to your code from intro_machinelearning_supervised.Rmd
+# There are A LOT of variables in this dataset, so feel free to explore whatever you think looks most interesting
+# Spend a little time on this looking at variables you think might be interesting later
+
+
+# Use the featurePlot() function to look at the variables (you won't be able to see them all at once)
+# You can color the plots based on species or genus to compare Choloepus to Bradypus
+
+
+# Validation & training sets ----------------------------------------------
+
+# Refer to your previous code to split your dataset into validation and training sets
+
+
